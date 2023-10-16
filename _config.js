f0d3ac3850755aee5d8ae7967bf52b4a577cb30a
maciekpalmowski <136691573+maciekpalmowski@users.noreply.github.com>
2023-10-17 00:38:57 +0200

    port

--- _config.js
+++ _config.js
@@ -1,6 +1,13 @@
 import lume from "lume/mod.ts";
 
-const site = lume();
+const port = Deno.env.get("PORT") ? Number(Deno.env.get("PORT")) : 8000;
+const site = lume({
+    server: {
+        port: port
+    }
+});
+
 site.ignore("README.md", "CHANGELOG.md", "node_modules");
+
 
 export default site;
